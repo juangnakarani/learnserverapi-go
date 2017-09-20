9f1427e3cba237a1607fdaf5767abb7638d9bc52
juangnakarani <juang.nakarani@gmail.com>
2017-09-20 10:32:55 +0700

    learn http handler

--- myserverapi.go
+++ myserverapi.go
@@ -1,9 +1,35 @@
 package main
 
  import (
+         "context"
          "fmt"
          "net/http"
  )
+ var keyValue = struct{}{}
+ 
+ func WithParamHandler(p string, h http.Handler) http.Handler {
+         return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                 //v := r.URL.Query().Get(p)
+                 v := "admin"
+                 h.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), keyValue, v)))
+         })
+ }
+ 
+ func MustAdminHandler(h http.Handler) http.Handler {
+         return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+ 
+                 if v, ok := r.Context().Value(keyValue).(string); ok && v == "admin" {
+                         h.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), keyValue, v)))
+                         return
+                 }
+ 
+                 http.Error(w, "You must have administrative permissions", http.StatusForbidden)
+ 
+         })
+ }
+ func AdminPage(w http.ResponseWriter, r *http.Request){
+        w.Write([]byte("Yes you are admin"))
+ }
 
  func Cars(w http.ResponseWriter, r *http.Request) {
          w.Header().Set("Content-Type", "application/json")
@@ -16,12 +42,15 @@ package main
  }
 
  func main() {
-
-         mux := http.NewServeMux()
+         port := ":7070"
+         //mux := http.NewServeMux()
          
-         mux.HandleFunc("/car", Cars)
-         fmt.Println("server run on 7070")
+         http.Handle("/car", http.HandlerFunc(Cars))
+        //  mux.HandleFunc("/admin", AdminPage)
+        http.Handle("/admin", WithParamHandler("token", MustAdminHandler(http.HandlerFunc(AdminPage))))
+         fmt.Printf("server run on %s\n", port)
          fmt.Println("/car for test json data")
-         http.ListenAndServe(":7070", mux)
+         fmt.Println("/admin for test admin page")
+         http.ListenAndServe(port, nil)
         
  }
